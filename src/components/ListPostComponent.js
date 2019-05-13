109607f69e759807483893acdc7b404b8543361d
kudr9tov <uladzislau.kudrashou@instinctools.ru>
2019-05-13 15:00:21 +0300

    styles

--- src/components/ListPostComponent.js
+++ src/components/ListPostComponent.js
@@ -31,7 +31,7 @@ class ListPostComponent extends React.Component {
     componentDidMount() {
         this.setState({isLoading: true});
 
-        instaService.getMedia(8400219101)
+        instaService.getMedia(10212391882)
             .then(data => this.setState({data: data['items'], isLoading: false}))
     }
 
