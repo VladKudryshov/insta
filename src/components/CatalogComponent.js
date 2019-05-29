0f534a50cd35746c7a7f30e51fa65c8ec3613cf3
kudr9tov <uladzislau.kudrashou@instinctools.ru>
2019-05-29 22:24:10 +0300

    responsetive

--- src/components/CatalogComponent.js
+++ src/components/CatalogComponent.js
@@ -11,7 +11,24 @@ class MainComponent extends Component {
 
         return (
             <>
-
+                <div className="admin-menu"></div>
+                <div className="content card">
+                    <div className="products">
+                        <div className="product">
+                            <div className="product-title">Product</div>
+                            <div className="product-price">
+                                <div className="number">12.12</div>
+                                <div className="currency">BYN</div>
+                            </div>
+                            <div className="product-image"></div>
+                            <div className="product-action">
+                                <i className="fas fa-receipt left p14 sml"></i>
+                                <i className="fas fa-shopping-basket basket p14 sml"></i>
+                                <i className="far fa-bookmark right p14 sml"></i>
+                            </div>
+                        </div>
+                    </div>
+                </div>
             </>
         );
     }
