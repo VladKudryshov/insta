c620d7dbe0a3ee748d9475a63d008ea67e710471
kudr9tov <uladzislau.kudrashou@instinctools.ru>
2019-05-31 12:41:22 +0300

    responsetive

--- src/components/catalog/ProductComponent.js
+++ src/components/catalog/ProductComponent.js
@@ -11,15 +11,16 @@ class ProductComponent extends Component {
 
     render() {
         return (
-            <div className="product">
+            <div className="product card">
                 <div className="product-title">{this.props.product.name}</div>
                 <div className="product-price">
                     <div className="number">{this.props.product.price}</div>
                     <div className="currency">BYN</div>
                 </div>
                 <div className="product-image"><img
-                    src="https://i5.walmartimages.ca/images/Large/428/5_r/6000195494285_R.jpg" alt=""/></div>
-                <div className="product-action">
+                    src={this.props.product.image} alt=""/></div>
+
+            <div className="product-action">
                     <i className="fas fa-receipt left p14 sml"></i>
                     <i className="fas fa-shopping-basket basket p14 sml"></i>
                     <i className="far fa-bookmark right p14 sml"></i>
